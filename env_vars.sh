6ee39fe4a03d636b14d019c5ce012dc6dc3cee47
Hakim Benyoucef <hakim.benyoucef@adlinktech.com>
2019-06-26 23:35:09 +0200

    Lesson5: commit env vars

---
+++ env_vars.sh
@@ -0,0 +1,8 @@
+#POSTGRES CREDENTIALS
+export POSTGRESQL_USERNAME=udagrambenyoucefdev
+export POSTGRESQL_PASSWORD=udagrambenyoucefdev
+export POSTGRESQL_DATABASE=udagrambenyoucefdev
+export POSTGRESQL_HOST=udagrambenyoucefdev.czwozcrqmqkz.us-east-1.rds.amazonaws.com
+export POSTGRESQL_AWS_REGION=us-east-1
+export POSTGRESQL_AWS_PROFILE=default
+export POSTGRESQL_AWS_MEDIA_BUCKET=udagram-benyoucef-dev
